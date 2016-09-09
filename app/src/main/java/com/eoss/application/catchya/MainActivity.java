e80bf3c055ec7c4e5bd980bb1eac0215ba5d81bf
eoss-th <contact@eoss-th.com>
2016-09-09 13:28:54 +0700

    NewPro

---
+++ app/src/main/java/com/eoss/application/catchya/MainActivity.java
@@ -0,0 +1,74 @@
+package com.eoss.application.catchya;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.login.LoginManager;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FacebookLogin";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+                    String uid = user.getUid();
+                    Log.d(name,photoUrl.toString());
+
+                } else {
+                    Intent myIntent = new Intent(MainActivity.this, Login.class);
+                    MainActivity.this.startActivity(myIntent);
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
+
+
+    }
+
+    public void logout(View v){
+        mAuth.signOut();
+        LoginManager.getInstance().logOut();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+}
